174b827ecdd81791afc006984a49b010b4d29700
rcornacchia <rlc2160@columbia.edu>
2017-10-12 01:41:45 -0400

    updated with ES6 classes for the third exercise

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -2,23 +2,23 @@
 // * `C` -> `G`
 // * `T` -> `A`
 // * `A` -> `U`
-var DnaTranscriber = function() {};
-
-DnaTranscriber.prototype.toRna = function(str) {
-  return str.split('').map(char => {
-    switch (char) {
-      case 'G':
-        return 'C';
-      case 'C':
-        return 'G';
-      case 'T':
-        return 'A';
-      case 'A':
-        return 'U';
-      default:
-        throw Error('Invalid input')
-    } 
-  }).join('');
+class DnaTranscriber {
+  toRna(str) {
+    return str.split('').map(char => {
+      switch (char) {
+        case 'G':
+          return 'C';
+        case 'C':
+          return 'G';
+        case 'T':
+          return 'A';
+        case 'A':
+          return 'U';
+        default:
+          throw Error('Invalid input')
+      } 
+    }).join('');
+  }
 }
 
 module.exports = DnaTranscriber;
